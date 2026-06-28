e0d9124feed9ee14db4a8497af2f8d62ca98c638
Kalyan <programmer143143@gmail.com>
2026-06-28 13:30:02 +0530

    Intialized the repo

---
+++ src/main/java/com/example/EcommerceEnd2End/EcommerceEnd2EndApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceEnd2End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceEnd2EndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceEnd2EndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcommerceEnd2End/EcommerceEnd2EndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceEnd2End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceEnd2EndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
